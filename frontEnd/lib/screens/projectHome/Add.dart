14c22f0f5501c6e6857aff0df5ea80ab52bce02d
sarra zaghbib <97798458+sarrazaghbib@users.noreply.github.com>
2023-01-17 21:20:51 +0100

    Update Add.dart

--- frontEnd/lib/screens/projectHome/Add.dart
+++ frontEnd/lib/screens/projectHome/Add.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:collabapp/resources/color_manager.dart';
 import '../QRCode/readqr.dart';
-import '../projects/AddProjectSaif.dart';
+import '../projects/AddProject.dart';
 import 'projectdashboard.dart';
 import './AddButtons.dart';
 
