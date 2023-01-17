e14824f11e020a1323effefd55b63828ef2bcb36
sarra zaghbib <97798458+sarrazaghbib@users.noreply.github.com>
2023-01-17 21:21:34 +0100

    Update AddButtons.dart

--- frontEnd/lib/screens/projectHome/AddButtons.dart
+++ frontEnd/lib/screens/projectHome/AddButtons.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:collabapp/resources/color_manager.dart';
-import 'package:collabapp/screens/projects/AddProjectSaif.dart';
+import 'package:collabapp/screens/projects/AddProject.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
